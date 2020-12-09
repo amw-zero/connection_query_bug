257d0bf6a01961e471d27e083f74e8bd4eb5fcd7
Alex Weisberger <alex.m.weisberger@gmail.com>
2020-12-08 22:55:26 -0500

    test property that exposes bug

--- test/hypothesis_test.rb
+++ test/hypothesis_test.rb
@@ -4,49 +4,47 @@ require 'test_helper'
 class HypothesisTest < ActiveSupport::TestCase
   include Hypothesis
   include Hypothesis::Possibilities
+
+  def stages
+    any element_of([:inquiry, :lease_executed])
+  end
+
+  def deals(tenant:)
+    built_as do
+      existing_deal_count = any integers(min: 0, max: 3)
+      existing_deal_count.times.map do
+        Deal.create(tenant: tenant, stage: stages)
+      end
+    end
+  end
+
+  def new_deals(tenant:)
+    built_as do
+      Deal.create(tenant: tenant, stage: stages)
+    end
+  end
   
   def test_hypo2
     hypothesis do
-      puts "========"
-      puts "  Frame"
-      puts "========="
       DatabaseCleaner.clean
-      puts "Starting Deal count: #{Deal.count}"
 
       tenant_count = any integers(min: 2, max: 4), name: 'Tenant Count'
       tenant_count.times do |i|
         Tenant.create(name: "Tenant #{i}")
       end
 
-      existing_deals = any(
-        built_as do
-          existing_deal_count = any integers(min: 0, max: 1)
-          existing_deal_count.times.map do
-            Deal.create(tenant: any(element_of(Tenant.all)), stage: any(element_of([:inquiry, :lease_executed])))
-          end
-        end,
-        name: 'Existing Deals'
-      )
+      tenant = Tenant.first
+      deals_for_tenant = any deals(tenant: tenant), name: 'Connected Deals'
+      other_deals = any deals(tenant: any(element_of(Tenant.all - [tenant]))), name: 'Existing Deals'
       
-      new_deal = any(
-        built_as do
-          Deal.create(tenant: Tenant.first, stage: any(element_of([:inquiry, :lease_executed])))
-        end, 
-        name: 'New Deal'
-      )
+      new_deal = any new_deals(tenant: tenant), name: 'New Deal'
 
-      puts; puts
-
-      if Connections.connection_exists_bug?(new_deal)
-        puts "Hello "
-        puts existing_deals.to_s
-        puts existing_deals.any? { |d| d.stage.to_sym == :lease_executed }
-        any_late_stage_deal = existing_deals.any? { |d| d.stage.to_sym == :lease_executed }
+      if Connections.connection_exists?(new_deal)
+        any_late_stage_deal = deals_for_tenant.any? { |d| d.stage.to_sym == :lease_executed }
         assert_equal true, any_late_stage_deal
       else
-        assert_equal true, existing_deals.all? { |d| d.stage.to_sym == :inquiry } || existing_deals.empty?
+        assert_equal true, deals_for_tenant.all? { |d| d.stage.to_sym == :inquiry }
       end
-      
     end    
   end
 end
