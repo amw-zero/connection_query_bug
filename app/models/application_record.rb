e427dc2d530cd2e83d9502936e0a9a4240278ac8
Alex Weisberger <alex.m.weisberger@gmail.com>
2021-01-23 10:29:40 -0500

    Separate attributes and associations for debug output

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,22 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def inspect_associations
+    self.class.reflect_on_all_associations.map do |a|
+      value = self.public_send(a.name)
+      { a.name.to_sym => value.id }
+    end
+  end
+
+  def foreign_keys
+    self.class.reflect_on_all_associations.map(&:foreign_key).compact
+  end
+
+  def inspect
+    str = "<#{self.class} #{id}: #{attributes.except('id', *foreign_keys)}"
+    assocs = inspect_associations
+    str << " | Associations: #{inspect_associations}>" unless assocs.empty?
+
+    str
+  end
 end
